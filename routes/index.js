9fbf76a1ce1024650dc1861471744c416242e0f4
Eslam nasser <in1146@fayoum.edu.eg>
2018-10-07 18:04:56 +0200

    Change gmail auth

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,13 @@ var router = express.Router();
 var fs = require('fs');
 var nodemailer = require('nodemailer');
 
-/* GET home page. */
+/* GET */
 router.get('/', function (req, res, next) {
     try {
         fs.readFile('github-webhook-response.json', function (err, buffer) {
             if (buffer && !err) {
                 var data = JSON.parse(buffer);
-                console.log('\n\n DATA FROM LOCAL FILE => ', data);
+                // console.log('\n\n DATA FROM LOCAL FILE => ', data);
                 res.json({
                     message: 'GitHub Data...',
                     data: data
@@ -28,81 +28,52 @@ router.get('/', function (req, res, next) {
     }
 });
 
+
+
+
+
+var transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {
+        user: 'nodemailer9@gmail.com', // Your email id
+        pass: 'Yeah its me' // Your password
+    },
+    tls: {
+        // do not fail on invalid certs
+        rejectUnauthorized: false
+    }
+});
+
+/* POST */
 router.post('/', function (req, res, next) {
-    console.log('\n\n\n\n\n GITHUB POST REQUEST');
+    // console.log('\n\n\n\n\n GITHUB POST REQUEST');
     console.log(req.body);
 
     var data = JSON.stringify(req.body);
     fs.writeFileSync('github-webhook-response.json', data);
 
-    var transporter = nodemailer.createTransport({
-        service: 'Gmail',
-        auth: {
-            user: 'nodemailer9@gmail.com',
-            pass: 'Yeah its me'
-        },
-        tls: {
-            rejectUnauthorized: false
-        }
-    });
 
     var mailOptions = {
-        from: `${repository.name} <eslam.nasser.dev@gmail.com>`,
+        from: 'Foo Bar ✔ <foobar@gmail.com>',
         to: 'eslam.nasser.dev@gmail.com',
-        subject: `${repository.full_name} Github Notification`,
-        text: 'You have a new notification!',
-        html: `
-			<div style='
-                width: 50%;
-                background-color: rgba(238, 238, 238, 0.3);
-                margin: auto;
-                position: relative;
-                padding-bottom: 20px;
-                box-shadow: 0 2px 2px 0 rgba(0,0,0,0.14), 0 1px 5px 0 rgba(0,0,0,0.12), 0 3px 1px -2px rgba(0,0,0,0.2);
-				'>
-				<h1 style='
-                    position: absolute;
-                    top: 0;
-                    left: 0;
-                    right: 0;
-                    margin: 0;
-                    background: #3498db;
-                    color: #FFF;
-                    padding: 10px;
-                    font-size: 15px;
-                '> You have a new message!</h1>
-				<ul>
-					<li style='margin: 2px auto;'><b>Sender Message is below:</li>
-				</ul>
-                <p style='
-                    background: #FFF;
-                    padding: 5px 10px;
-                    border: 1px solid #EEE;
-                    width: 85%;
-                    margin: auto;
-                '>
-                    Commit Name
-                </p>
-			</div>
-		`
+        subject: "Hello " + 'eslam.nasser.dev@gmail.com',
+        text: 'Hello ' + 'eslam.nasser.dev@gmail.com' + '✔',
+        html: "<p>Hello " + 'eslam.nasser.dev@gmail.com' + " </p>",
+        bcc: "eslam.nasser.dev@gmail.com"
     };
-
-    return transporter.sendMail(mailOptions, function (error, info) {
+    transporter.sendMail(mailOptions, function (error, info) {
         if (error) {
-            console.log('___Error: => ' + error);
-            res.json({
-                success: false,
-                error: error
-            });
+            console.log(error);
         } else {
-            console.log('Message Sent! ' + info.response);
-            res.sendStatus(200);
-            res.json({
-                success: true,
-                msg: info.response
-            });
+            console.log('Message sent: ' + info.response);
+            res.send(200);
         }
     });
+
+
+    return res.json({ done: true });
 });
 
 module.exports = router;
