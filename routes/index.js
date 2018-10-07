602cedc7b449d464ae01849648cfc2b6cf94f7de
Eslam nasser <in1146@fayoum.edu.eg>
2018-10-07 02:07:33 +0200

    Validate if a json file exist

--- routes/index.js
+++ routes/index.js
@@ -4,16 +4,21 @@ var fs = require('fs');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  var data = JSON.parse(fs.readFileSync('./github-webhook-response.json'));
-  res.json({
-    message: 'GitHub Data...',
-    data: data
-  });
+    var data = JSON.parse(fs.readFileSync('./github-webhook-response.json'));
+    if (data) {
+        return res.json({
+            message: 'GitHub Data...',
+            data: data
+        });
+    }
+    return res.json({
+        message: 'No Github Data!'
+    });
 });
 
 router.post('/', function (req, res, next) {
-  var data = JSON.stringify(req);
-  fs.writeFileSync('github-webhook-response.json', data);
+    var data = JSON.stringify(req);
+    fs.writeFileSync('github-webhook-response.json', data);
 });
 
 module.exports = router;
