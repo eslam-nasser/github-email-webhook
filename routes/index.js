87a9e3c3e1958b5afbf6f815c8b859edb8f09b97
Eslam nasser <in1146@fayoum.edu.eg>
2018-10-07 02:23:57 +0200

    Read json file async

--- routes/index.js
+++ routes/index.js
@@ -5,13 +5,15 @@ var fs = require('fs');
 /* GET home page. */
 router.get('/', function (req, res, next) {
     try {
-        var data = JSON.parse(fs.readFileSync('./github-webhook-response.json'));
-        if (data) {
+        fs.readFile('github-webhook-response.json', function (err, buffer) {
+            if (err) throw err;
+            var data = JSON.parse(buffer);
+            console.log('\n\n DATA FROM LOCAL FILE => ', data);
             return res.json({
                 message: 'GitHub Data...',
                 data: data
             });
-        }
+        });
     } catch (error) {
         return res.json({
             message: 'No Github Data!',
@@ -22,9 +24,9 @@ router.get('/', function (req, res, next) {
 
 router.post('/', function (req, res, next) {
     console.log('\n\n\n\n\n GITHUB POST REQUEST');
-    console.log(req);
+    console.log(req.body);
 
-    var data = JSON.stringify(req);
+    var data = JSON.stringify(req.body);
     fs.writeFileSync('github-webhook-response.json', data);
     return res.json({
         success: true
