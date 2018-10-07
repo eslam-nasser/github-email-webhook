91cde1c45dfa889944163dbbb4447a05ec0bd8c6
Eslam nasser <in1146@fayoum.edu.eg>
2018-10-07 02:00:42 +0200

    -Save Incoming github data to json file - Load the json file on GET request

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
+  var data = JSON.parse(fs.readFileSync('./github-webhook-response.json'));
   res.json({
-    message: 'Up and running ...',
+    message: 'GitHub Data...',
+    data: data
   });
+});
+
+router.post('/', function (req, res, next) {
+  var data = JSON.stringify(req);
+  fs.writeFileSync('github-webhook-response.json', data);
 });
 
 module.exports = router;
